4c3c275dafcffdea8fe388d1beb34d4dac07cfd1
mehreen2103 <mehreenansari2097@gmail.com>
2025-09-01 15:42:10 +0530

    Oop

---
+++ Java Oops/src/in/com/rays/oops/Automobile.java
@@ -0,0 +1,30 @@
+package in.com.rays.oops;
+
+public class Automobile {
+	
+	private String color;
+	private int speed;
+	private String make;
+	
+	public String getcolor() {
+		return this.color;
+	}
+	public void setcolor(String color) {
+		this.color = color;
+	}
+	
+	public int  getSpeed() {
+		return this.speed;
+	}
+	public void setSpeed(int Speed) {
+		this.speed = speed;
+	}
+	
+	public String getMake() {
+		return this.make;
+	}
+	public void setMake(String Make) {
+		this.make = make;
+	}
+
+}
