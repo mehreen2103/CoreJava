ed32eb2cfb26e27dd40e89d618f45961c71b8e66
mehreen2103 <mehreenansari2097@gmail.com>
2025-09-08 13:56:37 +0530

    done

---
+++ polymorphism/ICICIBank.java
@@ -0,0 +1,13 @@
+package in.com.rays.polymorphism;
+
+public class ICICIBank extends Bank{
+	
+	public double interestRate() {
+		return 9.75;
+	}
+	
+	public String getName() {
+		return "ICICI Bank";
+	}
+	
+}
