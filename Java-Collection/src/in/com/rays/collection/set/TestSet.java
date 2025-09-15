8149ea87ab7d23a371c416104750584d6d4b27ff
mehreen2103 <mehreenansari2097@gmail.com>
2025-09-15 14:14:36 +0530

    Set

---
+++ Java-Collection/src/in/com/rays/collection/set/TestSet.java
@@ -0,0 +1,28 @@
+package in.com.rays.collection.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestSet {
+	
+	public static void main(String[] args) {
+		
+		Set s = new HashSet();
+		
+		s.add('a');
+		s.add('s');
+		s.add('f');
+		s.add('A');
+		s.add('h');
+		s.add('H');
+		s.add('p');
+		s.add('t');
+		s.add('e');
+		s.add('S');
+		s.add('M');
+		
+		System.out.println(s);
+		
+	}
+
+}
